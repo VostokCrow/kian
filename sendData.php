d68f6715ae536b70ccf15fd0871b6e9b75e2579f
VostokCrow <48295020+VostokCrow@users.noreply.github.com>
2023-02-22 23:21:46 +0100

    Update sendData.php

--- sendData.php
+++ sendData.php
@@ -1,6 +1,15 @@
-<?php  
-
-print_r($_POST);
-
-?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+<?php 
+  
+ print_r($_POST);
+?>	
+</body>
+</html>
 
